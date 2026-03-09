b64fa9dbffda6b67282d43de64d68a15a15db48a
H1lp0p <sasha.iadr@gmail.com>
2026-03-09 15:12:34 +0300

    added Header; TabList; SingleSpaceLayout and PlaceholderSpace

--- apps/user/src/App.tsx
+++ apps/user/src/App.tsx
@@ -1,53 +1,73 @@
-import { AmountWithSymbol, BgText, BluredContainer, InlineCheckBox, Input, LinkButton, Logo } from "@fins/ui-kit";
+import { AmountWithSymbol, BgText, BluredContainer, Header, InlineCheckBox, Input, LinkButton, Logo, OnBlurContainer, SingleSpaceLayout, TabList, type Tab } from "@fins/ui-kit";
 import { useState } from "react";
 import { DEFAULT_CHARS } from "@fins/ui-kit";
 
 export default function App() {
 
   const [inputValue, setInputValue] = useState<string>("");
+  const [activeTab, setActiveTab] = useState<string>("1");
 
   const handleInputChange = (value: string) => {
     setInputValue(value);
   };
 
+
+  const tabs = [
+    { id: "1", label: "Index" },
+    { id: "2", label: "Transactions" },
+    { id: "3", label: "Loans" },
+  ];
+
+  const handleTabClick = (tab: Tab) => {
+    setActiveTab(tab.id);
+  };
+  
+  const activeTabData = tabs.find((tab) => tab.id === activeTab)!;
+
   return (
     <main className="bg-background">
+      <Header 
+        tabs={tabs} 
+        activeTab={activeTabData} 
+        onTabClick={handleTabClick} 
+        trailingContent={
+          <span className="text-title color-info" style={{ textAlign: "right" }}>Test trailing content</span>
+        }
+      />
       
       <BgText text="Register"/>
 
-        <BluredContainer 
-          className="ph-max pv-max gap-mid" 
-          style={{ 
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            zIndex: 1,
+        <SingleSpaceLayout>
+          {/* <div className="ph-mid pv-max gap-mid" style={{
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-          }}
-          >
-            <Logo textStyle="text-giant"/>
+          }}>
+              <Logo textStyle="text-giant"/>
+              <LinkButton text="link info" textClassName="text-title" variant="info" onClick={() => {}} />
+              <InlineCheckBox content="inline check box" status="checked" onClick={() => {}} />
+              <AmountWithSymbol 
+                amount={10000.122} 
+                symbol={DEFAULT_CHARS.DOLLAR} 
+                style={{
+                  width: "100%",
+                }}
+                />
+              <Input 
+              title="Input" 
+              placeholder="Test input" 
+              value={inputValue} 
+              onChange={handleInputChange}
+              textClassName="text-info"
+              trailingChar={DEFAULT_CHARS.DOLLAR} />
+          </div>
+          <OnBlurContainer className="ph-mid pv-mid gap-min" style={{width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
             <LinkButton text="link info" textClassName="text-title" variant="info" onClick={() => {}} />
-            <InlineCheckBox content="inline check box" status="checked" onClick={() => {}} />
-            <AmountWithSymbol 
-              amount={10000.122} 
-              symbol={DEFAULT_CHARS.DOLLAR} 
-              style={{
-                width: "100%",
-              }}
-              />
-            <Input 
-            title="Input" 
-            placeholder="Test input" 
-            value={inputValue} 
-            onChange={handleInputChange}
-            textClassName="text-info"
-            trailingChar={DEFAULT_CHARS.DOLLAR} />
-        </BluredContainer>
+          </OnBlurContainer> */}
+        </SingleSpaceLayout>
 
+        
     </main>
   );
 }
---
+++ packages/ui-kit/src/components/tabs/TabList.tsx
@@ -0,0 +1,37 @@
+import {type Tab} from "../../types/Tab";
+import type { TextStyleType } from "../../types/textStyleType";
+import "./tablist.css";
+
+interface TabListProps {
+    tabs: Tab[];
+    activeTab: Tab;
+    onTabClick: (tab: Tab) => void;
+    textClassName?: TextStyleType;
+    className?: string;
+}
+
+export function TabList({ tabs, activeTab, onTabClick, textClassName = "text-title", className = "" }: TabListProps) {
+    
+    const handleTabClick = (tab: Tab) => {
+        onTabClick(tab);
+    };
+
+    return (
+        <div className={`tab-list ${className}`}>
+            {tabs.map((tab, index) => (
+                <div key={index}>
+                    <span
+                        className={`tab-item ${textClassName}`} 
+                        onClick={() => handleTabClick(tab)}
+                        data-active={tab.id === activeTab.id}
+                    >
+                        {tab.label}
+                    </span>
+                    {index < tabs.length - 1 && (
+                        <span className={`color-info ${textClassName}`}>/</span>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
+}
